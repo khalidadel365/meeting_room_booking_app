ebe25c762dae52451f5e7a05ddba33d50fbbf4e6
Khalid Adel <khalidadel209@gmail.com>
2026-04-10 17:03:09 +0200

    feat(rooms): implement room remote data source

---
+++ lib/features/rooms/data/data_sources/room_remote_data_source.dart
@@ -0,0 +1,24 @@
+import 'package:meeting_room_booking_app/core/values/api_endpoints.dart';
+import 'package:meeting_room_booking_app/features/rooms/data/models/responses/room_dto.dart';
+
+import '../../../../core/api/api_service.dart';
+import '../models/room_response.dart';
+
+abstract class RoomRemoteDataSourceContract {
+  Future<List<RoomDto>> fetchRooms();
+}
+
+class RoomRemoteDataSourceImpl implements RoomRemoteDataSourceContract {
+  final ApiService apiService;
+
+  RoomRemoteDataSourceImpl(this.apiService);
+
+  @override
+  Future<List<RoomDto>> fetchRooms() async {
+    final response = await apiService.get(endPoint: ApiEndpoints.rooms);
+
+    final roomsResponse = RoomsResponse.fromJson(response);
+
+    return roomsResponse.data ?? [];
+  }
+}
