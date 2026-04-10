004030b5b47ce3ea6632a818a393a17ddd8fcdbe
Khalid Adel <khalidadel209@gmail.com>
2026-04-10 22:03:11 +0200

    feat(booking): implement create booking response

---
+++ lib/features/bookings/data/models/create_booking_response.dart
@@ -0,0 +1,15 @@
+import 'package:meeting_room_booking_app/features/bookings/data/models/responses/booking_dto.dart';
+
+class CreateBookingResponse {
+  final BookingDto? data;
+
+  CreateBookingResponse({this.data});
+
+  factory CreateBookingResponse.fromJson(Map<String, dynamic> json) {
+    return CreateBookingResponse(
+      data: json["data"] == null
+          ? null
+          : BookingDto.fromJson(json["data"]),
+    );
+  }
+}
